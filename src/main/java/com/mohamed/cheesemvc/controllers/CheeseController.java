db6e996f6d667219873153088a8e0302227ad037
mmoha4115 <mmohamed54d0qf@gmail.com>
2018-12-17 15:20:04 -0600

    completed class 4 and all exercises. completed cheeese mvc studio. todo- complete validation

--- src/main/java/com/mohamed/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/mohamed/cheesemvc/controllers/CheeseController.java
@@ -7,32 +7,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping(value = "cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String,String> cheeses = new HashMap<>();
 
     @RequestMapping(value = "")
     public String index(Model model){
 
         model.addAttribute("cheeses",cheeses);
         model.addAttribute("title","My Cheeses");
-
         return "cheese/index";
     }
 
     @RequestMapping(value = "add" , method = RequestMethod.GET)
-    public  String displayAddCheeeseForm(Model model){
+    public  String displayAddCheeeseForm(Model model,String error){
         model.addAttribute("title","Add Cheese");
+        model.addAttribute("error",error);
         return "cheese/add";
     }
 
     @RequestMapping(value = "add" , method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(Model model,@RequestParam String cheeseName, @RequestParam String description){
+        if(cheeseName.isEmpty()){
+            String error = "Cheese name must contain alphabets only and must not be empty!";
+            return displayAddCheeeseForm(model, error);
+        }
+        cheeses.put(cheeseName,description);
 //        redirect to /cheese
+        return "redirect:";
+    }
+
+    public boolean containsNumbers(String cheeseName){
+
+        return false;
+    }
+    @RequestMapping(value = "remove",method = RequestMethod.GET)
+    public String removeCheeseForm(Model model){
+        model.addAttribute("cheeses",cheeses);
+        model.addAttribute("title","Remove Cheese");
+
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove",method = RequestMethod.POST)
+    public String proccessRemoveCheeseForm(@RequestParam ArrayList<String> cheese){
+        for (String aCheese : cheese) {
+            cheeses.remove(aCheese,cheeses.get(aCheese));
+        }
+
         return "redirect:";
     }
 }
